a0c155aed2ae0ab0034d3778dbd2f6158fc71fa7
alexandr.shinkevich <alexandr.shinkevich@gmail.com>
2018-03-13 16:35:22 +0600

    add exclude for findbugs and add comments

--- web/src/main/java/eshop/controller/HomeServlet.java
+++ web/src/main/java/eshop/controller/HomeServlet.java
@@ -6,8 +6,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Home controller
+ */
 public class HomeServlet extends HttpServlet {
 
+    private static final long serialVersionUID = -3009993516674880490L;
+
+    /**
+     * forward the control to the index.html
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("html/index.html").forward(req, resp);
